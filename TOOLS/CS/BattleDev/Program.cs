9bcf1884355e022b47fa67f629ea6e364028c0ef
FIRLEJ Jerome <A119HS@login.axa>
2022-11-10 14:23:55 +0100

    feat(csharp): ajout de fonctions + launch net6

--- TOOLS/CS/BattleDev/Program.cs
+++ TOOLS/CS/BattleDev/Program.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-
-namespace BattleDev
+﻿namespace BattleDev
 {
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
     public class Program
     {
-
-        private int[] GetIntsLine() => Console.ReadLine().Split(" ").Select(v => Int32.Parse(v)).ToArray();
-        private int GetIntLine() => Int32.Parse(Console.ReadLine());
         public static void Main(string[] args)
         {
             string line;
@@ -19,5 +16,11 @@ namespace BattleDev
             //résultat
             Console.WriteLine();
         }
+
+        private static int[] GetIntsLine() => Console.ReadLine().Split(" ").Select(v => Int32.Parse(v)).ToArray();
+        
+        private static int GetIntLine() => Int32.Parse(Console.ReadLine());
+
+        private static bool CollectionEqual<T>(IEnumerable<T> coll1,IEnumerable<T> coll2 ) => Enumerable.SequenceEqual<T>(coll1, coll2);
     }
 }
